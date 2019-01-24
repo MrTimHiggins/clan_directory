ac8f5a9f1bf3ecdbcbf2094771c70f86e003a682
Tim Higgins <tim.j.higgins@gmail.com>
2019-01-23 23:37:15 -0800

    Add some basic seeds, add associations to family, clean up gemfile, and add awesome print

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,3 +1,5 @@
 class Family < ApplicationRecord
   belongs_to :clan
+  has_many :family_members
+  has_one :contact_info
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+if Rails.env == "development"
+  Clan.create(name: "Higgins Clan")
+  Family.create(name: "Tracy Family", clan_id: Clan.first.id)
+
+
+  9.times do |n|
+    FamilyMember.create(first_name: "firstname#{n}", last_name: "lastname#{n}", age: "1#{n}", family_id: Family.first.id)
+  end
+
+end
