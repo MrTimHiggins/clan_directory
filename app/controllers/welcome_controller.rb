22535b157cfa01f90997510f645fa83b651a9f03
Tim Higgins <tim.j.higgins@gmail.com>
2020-01-11 21:40:23 -0800

    Order by first name

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,9 @@ class WelcomeController < ApplicationController
     @q = params[:search]
 
     if @q
-      @family_members = FamilyMember.where("concat_ws(' ', first_name, last_name) ILIKE ?", "%#{@q.squish}%").includes(:contact_info)
+      @family_members = FamilyMember.where("concat_ws(' ', first_name, last_name) ILIKE ?", "%#{@q.squish}%").includes(:contact_info).order(:first_name)
     else
-      @family_members = FamilyMember.all.includes(:contact_info)
+      @family_members = FamilyMember.all.includes(:contact_info).order(:first_name)
     end
   end
 
